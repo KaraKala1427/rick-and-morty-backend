6f94e7363111b14f394df5398b1d494166d1bc55
KaraKala1427 <ernarerbol027@gmail.com>
2021-08-17 18:56:41 +0600

    pagination class replaced to AppServiceProvider

--- app/Repositories/CharacterRepository.php
+++ app/Repositories/CharacterRepository.php
@@ -14,7 +14,6 @@ class CharacterRepository implements CharacterRepositoryInterface
 {
     public function index($request)
     {
-//        $characters = Character::query()->where('name','LIKE',"%{$nameFilter}%")->get();
         $characters = Character::all();
         if ($request->has('status')){
             $characters = $characters->whereIn('status',$request->status);
@@ -26,20 +25,15 @@ class CharacterRepository implements CharacterRepositoryInterface
             $characters = $characters->whereIn('race',$request->race);
         }
         if($request->has('per_page')){
-            $characters = $this->paginate($characters, $request->per_page);
+            $characters = $characters->paginate($request->per_page);
         }
-
-//        if ($request->has('name')){
-//            $characters = $characters->where('name','LIKE',"%{$request->name}%")->get();
-//        }
+        if ($request->has('name')){
+            $characters = Character::query()->where('name','LIKE',"%{$request->name}%")->get();
+//            $characters = $characters->where('name','LIKE',"%{$request->name}%");
+        }
         return $characters;
     }
-    public function paginate($items, $perPage = 3, $page = null, $options = [])
-    {
-        $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
-        $items = $items instanceof Collection ? $items : Collection::make($items);
-        return new LengthAwarePaginator($items->forPage($page, $perPage), $items->count(), $perPage, $page, $options);
-    }
+
     public function get($id)
     {
         return Character::findOrFail($id);
@@ -53,17 +47,14 @@ class CharacterRepository implements CharacterRepositoryInterface
 
     public function update($id, $data)
     {
-        $character = Character::query()->findOrFail($id);
-        $character->update($data);
+        $character = $this->get($id)->update($data);
         return $character;
 
     }
     public function destroy($id)
     {
-        $character = Character::query()->findOrFail($id);
-        $character->delete();
+        $character = $this->get($id)->delete();
         return $character;
-
     }
 
 }

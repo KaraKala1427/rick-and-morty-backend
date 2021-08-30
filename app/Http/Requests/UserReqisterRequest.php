f510c4c503c832031487b1b00e98e535cbc4aecf
KaraKala1427 <ernarerbol027@gmail.com>
2021-08-30 13:58:54 +0600

    validation for phone changed

--- app/Http/Requests/UserReqisterRequest.php
+++ app/Http/Requests/UserReqisterRequest.php
@@ -25,7 +25,7 @@ class UserReqisterRequest extends FormRequest
     {
         return [
             'name' => 'required|string',
-            'phone' => 'required|string|unique:users,phone',
+            'phone' => 'required|regex:/[0-9]{3} [0-9]{3} [0-9]{4}/|unique:users,phone',
             'password' => 'required|string|min:8'
         ];
     }
